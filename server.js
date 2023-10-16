021ec086f42e342f5609304307d7242cb9843296
ValentineOO <varlomxg@gmail.com>
2023-10-16 16:09:56 +0100

    made some more changes

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello node API");
 });
+app.get("/blog", (req, res) => {
+  res.send("Hello baba yaga123");
+});
 
 app.listen(3000, () => {
   console.log(`Node API listening on port 3000`);
 }
-)
+) 
