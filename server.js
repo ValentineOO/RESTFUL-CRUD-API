3dd08e9e84550d4996f9f5c7f3c85c1c62abda94
ValentineOO <varlomxg@gmail.com>
2023-10-16 17:39:22 +0100

    restful api going smoothly

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello node API");
 });
 app.get("/blog", (req, res) => {
-  res.send("Hello baba yaga123");
+  res.send("Hello baba yaga");
 });
 
-app.listen(3000, () => {
-  console.log(`Node API listening on port 3000`);
-}
-) 
+mongoose
+  .connect(
+    "mongodb+srv://admin:King5man!@val-api.rbqit3a.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node API listening on port 3000`);
+    });
+  })
+  .catch(() => {
+    console.log(error);
+  });
